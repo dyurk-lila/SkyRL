3926f58130f500bdaa035855c40578883e015f8a
dyurk-lila <dyurk@lila.ai>
2026-08-22 00:50:48 +0000

    style: apply Black formatting

--- tests/train/dataset/test_preprocess.py
+++ tests/train/dataset/test_preprocess.py
@@ -716,6 +716,8 @@ def test_sample_support_none_when_not_provided(tokenizer):
         loss_masks=[[1]],
     )
     assert packed is None
+
+
 def test_routed_expert_tensor_carries_interleaved_layer_indices(tokenizer):
     moe_layers = (1, 3)
     routes = [
